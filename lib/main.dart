f15caf5e8a553898eb3cf627d394e4f3ff8ee630
Tanmay Deshkar <tdeshkar_be21@gamil.com>
2022-09-01 23:19:58 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flashchat/chat_screen.dart';
+import 'package:flutter/material.dart';
+import 'welcome_screen.dart';
+import 'login_screen.dart';
+import 'registration_screen.dart';
+import 'constants.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
